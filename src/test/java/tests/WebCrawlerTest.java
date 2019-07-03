619e0a35c3c68cf97f230e09156fb06a7a4b0a8b
asasimov <37741229+asasimov@users.noreply.github.com>
2019-07-03 23:22:24 +0300

    Рефакторинг

--- src/test/java/tests/WebCrawlerTest.java
+++ src/test/java/tests/WebCrawlerTest.java
@@ -8,10 +8,10 @@ import org.testng.annotations.Test;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.*;
 
 public class WebCrawlerTest extends TestBase {
 
@@ -24,14 +24,18 @@ public class WebCrawlerTest extends TestBase {
 
     @Test
     public void testWebCrawler(){
+        final String fileName = "src/test/resources/new.csv";
+        final String delimiter = ";";
+
+        this.createCSVFile(fileName, delimiter, "url", "productName", "authorName", "price", "link");
+
         wd.get("https://www.mann-ivanov-ferber.ru/books/allbooks/?booktype=audiobook");
 
         int productCount = 0;
         int retryCount = 0;
         while (true) {
-            WebElement element = wd.findElement(By.cssSelector("div.js-page-loader.page-loader"));
-            actions.moveToElement(element).build().perform();
-            ((JavascriptExecutor) wd).executeScript("window.scrollBy(0, -100);");
+            actions.moveToElement(wd.findElement(By.cssSelector("div.js-page-loader.page-loader"))).build().perform();
+            ((JavascriptExecutor) wd).executeScript("window.scrollBy(0, -250);");
 
             int size = wd.findElements(By.cssSelector("img.lego-book__cover-image")).size();
             if(size != productCount){
@@ -44,56 +48,56 @@ public class WebCrawlerTest extends TestBase {
         }
         logger.info("Product count = " + productCount);
 
-        //получаем ссылки на все продукты
-        List<String> links = wd.findElements(By.xpath("//div[contains(@class, 'lego-book')]/a")).stream()
-                .map(e -> e.getAttribute("href"))
-                .collect(Collectors.toList());
-
-        //данные продуктов
-        List<List<String>> rows = new ArrayList<>();
-
-        for (String url : links){
+        List<String> links = this.getProductList();
+        for (String url : links) {
             wd.navigate().to(url);
-
-            //название
-            String productName = wd.findElement(By.xpath("//h1[@class='header active p-sky-title']/span")).getText();
-
-            //автор
-            String authorName = wd.findElement(By.cssSelector("div.authors")).findElements(By.cssSelector("a > span"))
-                    .stream().map(WebElement::getText).collect(Collectors.toList())
-                    .stream().collect(Collectors.joining(" "));
-
-            //цена
-            String price = wd.findElement(By.xpath("//div[@data-type='audiobook']")).getAttribute("data-price");
-
-            //ссылка для скачивания фрагмента
-            String link = "";
-            try {
-                link = wd.findElement(By.cssSelector("a.nkk-file-download__link[href$='.pdf']")).getAttribute("href");
-            } catch (NoSuchElementException ex){}
-
-            //сохраняем данные
-            rows.add(Arrays.asList(url, productName, authorName, price, link));
+            List<String> rowData = Arrays.asList(url, getProductName(), getAuthorsName(), getPrice(), getDownloadLink());
+            this.addRowToFile(fileName, delimiter, rowData);
         }
+    }
 
-        //запись в файл
-        final String delimiter = ";";
-        try(FileWriter csv = new FileWriter("src/test/resources/new.csv")){
-            csv.append("url");
-            csv.append(delimiter);
-            csv.append("productName");
-            csv.append(delimiter);
-            csv.append("authorName");
-            csv.append(delimiter);
-            csv.append("price");
-            csv.append(delimiter);
-            csv.append("link");
+    private void createCSVFile(String pathToFile, String delimiter, String... fieldNames){
+        try(FileWriter csv = new FileWriter(pathToFile, true)) {
+            csv.append(String.join(delimiter, fieldNames));
             csv.append("\n");
+        } catch (IOException ex) {
+            logger.error("Could not create file - " + pathToFile);
+        }
+    }
 
-            for (List<String> rowData : rows) {
-                csv.append(String.join(delimiter, rowData));
-                csv.append("\n");
-            }
-        } catch (IOException e){}
+    private void addRowToFile(String pathToFile, String delimiter, List<String> rows){
+        try(FileWriter csv = new FileWriter(pathToFile, true)){
+            csv.append(String.join(delimiter, rows));
+            csv.append("\n");
+        } catch (IOException e){
+            logger.warn("Failed to add line to file.");
+        }
+    }
+
+    private List<String> getProductList(){
+        return wd.findElements(By.xpath("//div[contains(@class, 'lego-book')]/a")).stream()
+                .map(e -> e.getAttribute("href")).collect(toList());
+    }
+
+    private String getAuthorsName() {
+        return wd.findElement(By.cssSelector("div.authors")).findElements(By.cssSelector("a > span"))
+                .stream().map(WebElement::getText).collect(toList())
+                .stream().collect(joining(" "));
+    }
+
+    private String getProductName(){
+        return wd.findElement(By.xpath("//h1[@class='header active p-sky-title']/span")).getText();
+    }
+
+    private String getPrice(){
+        return wd.findElement(By.xpath("//div[@data-type='audiobook']")).getAttribute("data-price");
+    }
+
+    private String getDownloadLink(){
+        try {
+            return wd.findElement(By.cssSelector("a.nkk-file-download__link[href$='.pdf']")).getAttribute("href");
+        } catch (NoSuchElementException ex){
+            return "";
+        }
     }
 }
