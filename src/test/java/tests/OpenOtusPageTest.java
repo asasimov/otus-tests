efde51e30ce0ba063f3949751a8dbe560d434aff
Alex <alex@MacBook-Pro-Alex.local>
2019-09-05 23:41:19 +0300

    Подключен Allure, добавлена интеграция с Jenkins

--- src/test/java/tests/OpenOtusPageTest.java
+++ src/test/java/tests/OpenOtusPageTest.java
@@ -1,5 +1,7 @@
 package tests;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Step;
 import org.testng.annotations.Test;
 
 import static org.hamcrest.CoreMatchers.containsStringIgnoringCase;
@@ -8,12 +10,18 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class OpenOtusPageTest extends TestBase {
 
     @Test
+    @Description("Открыть страницу Otus.ru")
     public void testOpenOtusPage(){
         logger.info("Test started.");
 
         wd.navigate().to("https://otus.ru");
-        assertThat(wd.getTitle(), containsStringIgnoringCase("otus"));
+        this.checkTitle("otus");
 
         logger.info("Test completed.");
+    }
+
+    @Step("Проверяем, что заголовок содержит {expTitle}")
+    private void checkTitle(String expTitle){
+        assertThat(wd.getTitle(), containsStringIgnoringCase(expTitle));
     }
 }
